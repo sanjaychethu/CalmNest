8ba72b1341d5214539897c7f11020fa87de58249
sanjaychethu <121676551+sanjaychethu@users.noreply.github.com>
2023-09-23 21:51:15 +0530

    files uploaded

---
+++ style.js
@@ -0,0 +1,30 @@
+
+          var playBtn=document.getElementById("playBtn");
+          const wavesurfer = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: 'white',
+      progressColor: 'light green',
+      barWidth: 4,
+      responsive:true,
+      height:90,
+      bearRadius:4
+    
+      
+    })
+    wavesurfer.load('musics/10 Minute Mindfullness Meditation.mp3');
+    playBtn.onclick=function(){
+      wavesurfer.playPause();
+      if(playBtn.src.includes("images/kisspng-youtube-play-button-clip-art-pause-button-5ac1a853c6c212.9168316215226409798141.png")){
+        playBtn.src="images/pause-icon-29584.png";
+      }else{
+        playBtn.src="images/kisspng-youtube-play-button-clip-art-pause-button-5ac1a853c6c212.9168316215226409798141.png";
+    
+      }
+    }
+    wavesurfer.on('finsh', function(){
+      playBtn.src="images/kisspng-youtube-play-button-clip-art-pause-button-5ac1a853c6c212.9168316215226409798141.png";
+      wavesurfer.stop();
+    
+    
+    })
+    
